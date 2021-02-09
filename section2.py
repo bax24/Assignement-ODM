150659c702469a3e159f651db0df500ed7ddaeef
MartinL97 <49979727+MartinL97@users.noreply.github.com>
2021-02-09 17:22:06 +0100

    Add files via upload

--- section2.py
+++ section2.py
@@ -1,33 +1,33 @@
 import numpy as np
 import functions as f
 
-
-def expected_return(state,grid,gamma,N,stocha):
+def expected_return(state,grid,gamma,N,stocha,matrix):
 	
-	policy = 1 # random movement (any other integer means 'always right')
+	policy = 2 # random movement (any other integer means 'always right')
 	
 	if N == 0:
 		return 0
 	else:
-		w = np.array([0])
-		if stocha:
-			np.random.uniform(0,1,1000)
-		
-		J_N_values = np.zeros(w.size)
-		
-		for i in range(w.size):
-			if(stocha and w[i] >= 0.5):
-				step = f.move(state, 'none', grid)
-				rew = f.get_reward(grid, (0,0))
+		m = matrix[N-1][state[0]][state[1]]
+		if not m == np.inf:
+			return m
+		else:
+			step = f.move(state, f.get_policy(policy), grid)
+			rew = f.get_reward(grid, step[3])
+			
+			if not stocha:
+				m = rew + gamma*expected_return(step[3],grid,gamma,N-1,stocha,matrix)
+				matrix[N-1][state[0]][state[1]] = m
+				return m
 			else:
-				step = f.move(state,  f.get_policy(policy), grid)
-				rew = f.get_reward(grid, step[3])
+				exp_rew = (-3 + rew)/2
+				exp_ret_no_disturb = expected_return(step[3],grid,gamma,N-1,stocha,matrix)
+				exp_ret_disturb = expected_return((0, 0),grid,gamma,N-1,stocha,matrix)
+				
+				m = exp_rew + gamma*(exp_ret_no_disturb + exp_ret_disturb)/2
+				matrix[N-1][state[0]][state[1]] = m
+				return  m
 			
-			J_N_values[i] = rew + gamma*expected_return(step[3],grid,gamma,N-1,stocha)
-			
-		return np.mean(J_N_values)
-			
-	
 
 
 # ---------- MAIN --------------
@@ -50,11 +50,15 @@ if __name__ == "__main__":
 	N = 916 # above that gamma**N < 0.0001 
 	
 	# Deterministic: stocha = False; Stochastic: stocha = True
-	stocha = True
+	stocha = False
+	
+	dyn_matrix = np.full((N,domain.shape[0],domain.shape[1]),np.inf)
+	
+	#J = expected_return(init,domain,gamma,N,stocha,dyn_matrix)
 	
 	J_values = np.zeros((5,5))
 	for i in range(5):
 		for j in range(5):
-			state = (i, j)
-			
-			J_values[i,j] = expected_return(state,domain,gamma,N,stocha)
+ 			state = (i, j)
+ 			
+ 			J_values[i,j] = expected_return(state,domain,gamma,N,stocha,dyn_matrix)
